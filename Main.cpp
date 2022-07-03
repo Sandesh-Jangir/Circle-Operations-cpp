e5a4dab1cc51c151dcb6dfc13510d74259f88888
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-07-03 11:36:32 +0530

    Increased Codes readability and Updated I/O interface

--- Main.cpp
+++ Main.cpp
@@ -2,11 +2,19 @@
 #include <math.h>
 using namespace std;
 
+// Declaring Functions
 float circumfurence(float);
 float area(float);
 
 int main(){
     float radius;
+    
+    // IO Interface
+    char input[20];
+    cout << "Please select one of the desired operations : "<< endl;
+    cout << "Circumfurence" << endl << "Area";
+    cin >> input;
+
     cout << "Enter Radius of the Circle : ";
     cin >> radius;
     cout << "Circumfurence : " << circumfurence(radius) << endl;
@@ -14,6 +22,7 @@ int main(){
     return 0;
 }
 
+// Defining Functions
 float circumfurence(float r){
     float circumfurence = 2*M_PI*r;
     return circumfurence;
